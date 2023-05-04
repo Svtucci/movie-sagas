6761830084ee3b8dc1c48014c25dade4ce2a9274
Stephen Vertucci <steve.vertucci@gmail.com>
2023-05-04 17:51:55 -0400

    added onclick function when clicking image that will bring you to details page for the id of img being clicked

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from 'react-redux'; 
+import React from 'react';
+
+function Details () {
+    
+    return(
+        <>
+        <h3>DETAILS!</h3>
+        <div>
+
+        </div>
+        </>
+    )
+}
+
+export default Details; 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,14 +2,20 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 
-function MovieList() {
+import { useHistory } from 'react-router-dom'; 
 
+function MovieList() {
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
+
+    const showDetails = (id) => {
+        history.push(`/details/${id}`); 
+    }
 
     return (
         <main>
@@ -19,7 +25,13 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img 
+
+                            src={movie.poster} 
+                            alt={movie.title} 
+                            onClick={() => showDetails(movie.id)}
+                            
+                            />
                         </div>
                     );
                 })}
